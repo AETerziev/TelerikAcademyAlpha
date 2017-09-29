10989b86468f24e8431005f1d5aa3c305c6d28c8
Atanas Terziev <djnasi4@gmail.com>
2017-09-30 00:20:10 +0300

    Added tasks to JS homework

--- Module 3/JS Homeworks/Functions/GetLargestNumber.js
+++ Module 3/JS Homeworks/Functions/GetLargestNumber.js
@@ -1,7 +1,7 @@
-function getLargestNumber(args){
+let  getLargestNumber=((args)=>{
     let getMax=((a,b)=>{
         return a<b?b:a;
     });
     let arr=args.split(' ');
     console.log(getMax(getMax(+arr[0],+arr[1]),+arr[2]));
-}    
+});    
